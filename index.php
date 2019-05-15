3b5eb95642f2d0cdd1d926ac0ec47d2b1ed8cdc4
catalingriu <50585115+catalingriu@users.noreply.github.com>
2019-05-15 15:26:44 +0300

    nam

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> EliteChim | Pagina Principala </title>
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="utf-8"/>
+		
+		<!--<link rel="stylesheet" type="text/css" href="CSS/.css">
+		<link rel="stylesheet" type="text/css" href="CSS/.css">-->
+	</head>
+		wtf2.0
+	<body>
+		
+	</body>
+</html>
